1c7e1b35cbaf8c4bb70c693187184b34f6702f4f
Алиса <sealisaa@yandex.ru>
2023-03-28 18:03:48 +0300

    add suggestAddingPhotos

--- src/main/java/baraholkateam/command/NewAdvertisementCommand.java
+++ src/main/java/baraholkateam/command/NewAdvertisementCommand.java
@@ -1,8 +1,17 @@
 package baraholkateam.command;
 
+import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
 import org.telegram.telegrambots.meta.api.objects.Chat;
 import org.telegram.telegrambots.meta.api.objects.User;
+import org.telegram.telegrambots.meta.api.objects.replykeyboard.ReplyKeyboardMarkup;
+import org.telegram.telegrambots.meta.api.objects.replykeyboard.buttons.KeyboardButton;
+import org.telegram.telegrambots.meta.api.objects.replykeyboard.buttons.KeyboardRow;
 import org.telegram.telegrambots.meta.bots.AbsSender;
+import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
+
+import java.util.ArrayList;
+import java.util.List;
+
 
 public class NewAdvertisementCommand extends Command {
     // TODO дописать первое действие при создании объявления
@@ -16,6 +25,38 @@ public class NewAdvertisementCommand extends Command {
 
     @Override
     public void execute(AbsSender absSender, User user, Chat chat, String[] strings) {
-        sendAnswer(absSender, chat.getId(), this.getCommandIdentifier(), user.getUserName(), NEW_AD);
+        SendMessage message = suggestAddingPhotos(chat.getId());
+        try {
+            absSender.execute(message);
+        } catch (TelegramApiException e) {
+            throw new RuntimeException(e);
+        }
+//        sendAnswer(absSender, chat.getId(), this.getCommandIdentifier(), user.getUserName(), NEW_AD);
+    }
+
+    public SendMessage suggestAddingPhotos(Long chatId) {
+        ReplyKeyboardMarkup replyKeyboardMarkup = new ReplyKeyboardMarkup();
+
+        KeyboardButton addPhotosButton = new KeyboardButton();
+        addPhotosButton.setText("Добавить фотографии");
+
+        KeyboardButton mainMenuButton = new KeyboardButton();
+        mainMenuButton.setText("Главное меню");
+
+        KeyboardRow keyboardFirstRow = new KeyboardRow();
+        keyboardFirstRow.add(addPhotosButton);
+        keyboardFirstRow.add(mainMenuButton);
+
+        List<KeyboardRow> keyboardRows = new ArrayList<>();
+        keyboardRows.add(keyboardFirstRow);
+
+        replyKeyboardMarkup.setKeyboard(keyboardRows);
+        replyKeyboardMarkup.setResizeKeyboard(true);
+
+        SendMessage message = new SendMessage();
+        message.setChatId(chatId);
+        message.setText("Добавьте фотографии");
+        message.setReplyMarkup(replyKeyboardMarkup);
+        return message;
     }
 }
