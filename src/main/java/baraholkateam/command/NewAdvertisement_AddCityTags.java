a65988290df7cf301df0bbadded00c58a95db41b
Алиса <sealisaa@yandex.ru>
2023-04-03 23:48:53 +0300

    remove unused imports

--- src/main/java/baraholkateam/command/NewAdvertisement_AddCityTags.java
+++ src/main/java/baraholkateam/command/NewAdvertisement_AddCityTags.java
@@ -1,15 +1,11 @@
 package baraholkateam.command;
 
-import baraholkateam.util.State;
 import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
 import org.telegram.telegrambots.meta.api.objects.Chat;
 import org.telegram.telegrambots.meta.api.objects.Message;
 import org.telegram.telegrambots.meta.api.objects.User;
 import org.telegram.telegrambots.meta.api.objects.replykeyboard.InlineKeyboardMarkup;
-import org.telegram.telegrambots.meta.api.objects.replykeyboard.ReplyKeyboardMarkup;
 import org.telegram.telegrambots.meta.api.objects.replykeyboard.buttons.InlineKeyboardButton;
-import org.telegram.telegrambots.meta.api.objects.replykeyboard.buttons.KeyboardButton;
-import org.telegram.telegrambots.meta.api.objects.replykeyboard.buttons.KeyboardRow;
 import org.telegram.telegrambots.meta.bots.AbsSender;
 import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
 
