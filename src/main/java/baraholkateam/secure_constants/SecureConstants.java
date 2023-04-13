af97260f03d49b570cb65d89da04b7ddbdd34507
AlexanderKiselyov <a_kiselyov-snz@mail.ru>
2023-04-13 11:24:24 +0300

    Для компиляции

--- src/main/java/baraholkateam/secure_constants/SecureConstants.java
+++ src/main/java/baraholkateam/secure_constants/SecureConstants.java
@@ -3,6 +3,10 @@ package baraholkateam.secure_constants;
 public final class SecureConstants {
     public static final String CREATE_TABLES = "";
     public static final String INSERT_NEW_ADVERTISEMENT = "";
+    public static final String ASK_ACTUAL_ADVERTISEMENTS = "";
+    public static final String REMOVE_ADVERTISEMENT = "";
     public static final String TAGS_SEARCH = "";
+    public static final String UPDATE_ATTEMPT_NUMBER = "";
+    public static final String UPDATE_NEXT_UPDATE_TIME = "";
     public static final String REMOVE_ALL_DATA = "";
 }
