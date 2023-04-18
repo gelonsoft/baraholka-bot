79c99b8593b30af34771e663e212cbc591aa210f
Alex-Birdov <114806823+Alex-Birdov@users.noreply.github.com>
2023-04-18 22:24:28 +0300

    Исправлены конфликты в фобъедененных файлах

--- src/main/java/baraholkateam/secure_constants/SecureConstants.java
+++ src/main/java/baraholkateam/secure_constants/SecureConstants.java
@@ -1,37 +1,7 @@
 package baraholkateam.secure_constants;
 
 public final class SecureConstants {
-    public static final String CREATE_TABLES = """
-            CREATE TABLE IF NOT EXISTS "Advertisement"
-            (
-                chat_id bigint NOT NULL,
-                message_id bigint NOT NULL,
-                tags character(100),
-                price bigint,
-                description character(100),
-                contacts character(100),
-                creation_time bigint,
-                next_update_time bigint,
-                PRIMARY KEY (chat_id, message_id)
-            );""";
-    public static final String INSERT_NEW_ADVERTISEMENT = """
-            INSERT INTO "Advertisement" VALUES (?, ?, ?, ?, ?, ?, ?, ?);""";
-    public static final String TAGS_SEARCH = """
-            SELECT message_id
-            FROM "Advertisement"
-            WHERE ? <@ regexp_split_to_array(tags, ' ')
-            LIMIT ?;""";
-    public static final String REMOVE_ALL_DATA = """
-            DELETE * FROM "Advertisement";""";
 
-    public static final String USER_ADS = """
-            SELECT * FROM "Advertisement"
-            WHERE chat_id = ?;
-            """;
-    public static final String DELETE_AD = """
-            DELETE FROM "Advertisement"
-            WHERE message_id = ?;
-            """;
     public static final String CREATE_TABLES = """
         CREATE TABLE IF NOT EXISTS "Advertisement"
         (
@@ -45,7 +15,18 @@ public final class SecureConstants {
             PRIMARY KEY (chat_id, message_id)
         );""";
     public static final String INSERT_NEW_ADVERTISEMENT = """
-        INSERT INTO "Advertisement" VALUES (?, ?, ?, ?, ?, ?, ?);""";
+            INSERT INTO "Advertisement" VALUES (?, ?, ?, ?, ?, ?, ?, ?);""";
+    public static final String REMOVE_ALL_DATA = """
+            DELETE * FROM "Advertisement";""";
+
+    public static final String USER_ADS = """
+            SELECT * FROM "Advertisement"
+            WHERE chat_id = ?;
+            """;
+    public static final String DELETE_AD = """
+            DELETE FROM "Advertisement"
+            WHERE message_id = ?;
+            """;
     public static final String TAGS_SEARCH = """
         SELECT message_id
         FROM "Advertisement"
@@ -67,6 +48,4 @@ public final class SecureConstants {
         UPDATE "Advertisement"
         SET next_update_time = ?
         WHERE chat_id = ? AND message_id = ?;""";
-    public static final String REMOVE_ALL_DATA = """
-        DELETE FROM "Advertisement";""";
 }
