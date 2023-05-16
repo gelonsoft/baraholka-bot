1d18032dec585172bf25bd6a2d058a4f357e80c9
Alisa Seletskaya <sealisaa@yandex.ru>
2023-05-16 16:54:17 +0300

    fixes

--- web/react-app/src/components/NewAd.js
+++ web/react-app/src/components/NewAd.js
@@ -18,7 +18,8 @@ class NewAd extends React.Component {
     }
 
     componentDidMount() {
-        let userData = localStorage.getItem('userData');
+        //let userData = localStorage.getItem('userData');
+        let userData = {"auth_date":1684051188,"first_name":"Алиса","hash":"afc6a8181ae6eb8f494551c94c39a63fae2835470210428556f8db7f54b66603","id":538160964,"last_name":"Селецкая","photo_url":"https://t.me/i/userpic/320/Uim0VYUr3WRDc7ofnIj40wRzPe1L7t63Nv0FXKqydjM.jpg","username":"sealisaa"};
         RequestService.getTags(userData).then((response) => {
             if (response.data) {
                 this.setState({
@@ -47,7 +48,8 @@ class NewAd extends React.Component {
         console.log(e);
         e.preventDefault();
         const NEW_AD_URL = "http://localhost:8080/api/add_advertisement";
-        let userData = JSON.parse(localStorage.getItem('userData'));
+        //let userData = JSON.parse(localStorage.getItem('userData'));
+        let userData = {"auth_date":1684051188,"first_name":"Алиса","hash":"afc6a8181ae6eb8f494551c94c39a63fae2835470210428556f8db7f54b66603","id":538160964,"last_name":"Селецкая","photo_url":"https://t.me/i/userpic/320/Uim0VYUr3WRDc7ofnIj40wRzPe1L7t63Nv0FXKqydjM.jpg","username":"sealisaa"};
         let photos = [];
         for (let i = 0; i < this.state.chosenPhotos.length; i++) {
             const reader = new FileReader();
@@ -98,7 +100,7 @@ class NewAd extends React.Component {
         axios({
             method: "post",
             url: NEW_AD_URL,
-            data: body,
+            data: body.toJSON,
             headers: { "Content-Type": "application/json" },
         })
         .then(function (response) {
