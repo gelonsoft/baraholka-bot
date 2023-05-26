85bc8e974e510b56e7855a39c3c4124c150bf665
LyashenkoValeria <leovaldez00@mail.ru>
2023-05-26 23:44:01 +0300

    Fixed localStorage and blur bugs

--- web/react-app/src/services/Contact.js
+++
@@ -1,9 +0,0 @@
-import React from "react";
-import '../style/style.css';
-
-export default class Contact extends React.Component {
-    ref = this.props.contact.at(0) === "@" ? "https://t.me/"+this.props.contact.substring(1) : this.props.contact
-    render() {
-        return <a className="ref-color" href={this.ref}>{this.props.contact}</a>
-    }
-}
