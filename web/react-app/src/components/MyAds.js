f11abe03f3362750677b898afb3c9e3d601cf1c0
LyashenkoValeria <leovaldez00@mail.ru>
2023-05-28 22:10:40 +0300

    Ad rendering moved to a separate class

--- web/react-app/src/components/MyAds.js
+++ web/react-app/src/components/MyAds.js
@@ -1,13 +1,10 @@
 import React from 'react';
 import '../style/style.css';
-import ReactSpoiler from "react-spoiler";
 import RequestService from "../services/RequestService";
-import ReadMoreReact from 'read-more-react';
 import Rodal from 'rodal';
 import '../style/rodal.css';
-import AliceCarousel from "react-alice-carousel";
 import ReactLoading from "react-loading";
-import Photo from "../services/Photo";
+import Ad from "../services/Ad";
 
 
 class MyAds extends React.Component {
@@ -74,20 +71,6 @@ class MyAds extends React.Component {
 }
 
 class FoundAds extends React.Component {
-    constructor(props) {
-        super(props);
-        this.handleBlurClick = this.handleBlurClick.bind(this);
-        this.state = {clickable: false};
-    }
-    listOfPhotos = this.props.photos.map((photo, index) => <Photo key={index} photo={photo}/>)
-    checkPrice = this.props.price == null ? "none" : "block";
-    checkPhone = this.props.phone == null ? "none" : "block";
-
-    handleBlurClick() {
-        const currentClickable = this.state.clickable;
-        this.setState({clickable: !currentClickable});
-    }
-
     render() {
         return (<div className="ad__form">
             <div className="main__form-row">
@@ -95,40 +78,15 @@ class FoundAds extends React.Component {
                 <Dialog id={this.props.id} delete={(id) => this.props.delete(id)}></Dialog>
             </div>
 
-            <div className="add__content">
-                <div className="ad__photo">
-                    <Carousel photos={this.listOfPhotos}/>
-                </div>
-                <div className="text__padding">
-                    <div className="ad-tags">{this.props.tags}</div>
-                    <span style={{display: this.checkPrice}}
-                          className="ad-bottom-padding">Цена: {this.props.price} руб.</span>
-                    <ReadMoreReact text={`Описание: ${this.props.description}`}
-                                   min={150}
-                                   ideal={150}
-                                   max={200}
-                                   readMoreText="Подробнее"/>
-                    <div>--------------------------------------------------------</div>
-                    <ReactSpoiler blur={10} hoverBlur={8} onClick={this.handleBlurClick}>
-                        <span style={{display: this.checkPhone}}>Номер телефона: {this.props.phone}</span>
-                        <div>Контакты:</div>
-                        {this.props.contacts.map((contact) => {
-                            const ref = contact.at(0) === "@" ? "https://t.me/"+contact.substring(1) : contact
-                            return <a className={this.state.clickable ? "enabled ref-color" : "disabled"}
-                                      href={ref}>{contact}<br/></a>
-                        })}
-                    </ReactSpoiler>
-                </div>
-            </div>
+            <Ad key={this.props.key} id={this.props.id}
+                photos={this.props.photos}
+                tags={this.props.tags}
+                price={this.props.price}
+                description={this.props.description} phone={this.props.phone}
+                contacts={this.props.contacts}
+            ></Ad>
         </div>);
     }
-}
-
-function Carousel(props) {
-    const listOfPhotos = props.photos
-    return (
-        <AliceCarousel autoWidth={false} mouseTracking items={listOfPhotos}/>
-    );
 }
 
 const Example = () => (
