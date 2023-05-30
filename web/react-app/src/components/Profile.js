ef1407766467dfe38bdbec4e18664b16fc711181
Alisa Seletskaya <sealisaa@yandex.ru>
2023-05-30 15:43:05 +0300

    add default profile pic

--- web/react-app/src/components/Profile.js
+++ web/react-app/src/components/Profile.js
@@ -11,8 +11,10 @@ class Profile extends React.Component {
         let userData = JSON.parse(localStorage.getItem('userData'));
         return (
             <div className="profile">
-                <img className="profile-photo" alt="profile photo"
-                     src={userData.photo_url ? userData.photo_url : avatar}></img>
+                <div className="profile-photo-container">
+                    <img className="profile-photo" alt="profile photo"
+                         src={userData.photo_url ? userData.photo_url : avatar}></img>
+                </div>
                 <div className="profile-info">
                     <div className="user-name">{userData.first_name} {userData.last_name}</div>
                     <div className="user-login">{userData.username}</div>
