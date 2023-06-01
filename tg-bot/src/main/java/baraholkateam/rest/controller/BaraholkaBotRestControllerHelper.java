b8628a76a9d9829e9362ac5e661a9ac4f72b327c
AlexanderKiselyov <a_kiselyov-snz@mail.ru>
2023-06-01 18:53:46 +0300

    revert changes

--- tg-bot/src/main/java/baraholkateam/rest/controller/BaraholkaBotRestControllerHelper.java
+++ tg-bot/src/main/java/baraholkateam/rest/controller/BaraholkaBotRestControllerHelper.java
@@ -138,9 +138,6 @@ public class BaraholkaBotRestControllerHelper {
         if (!Objects.equals(priceString, "null")) {
             price = Long.parseLong(priceString);
         }
-        Long userId = Long.parseLong(json.get("id").asText());
-        String description = json.get("description").asText();
-        Long price = Long.parseLong(json.get("price").asText());
 
         return new CurrentAdvertisement(userId, description, tags, price, phone, contacts);
     }
