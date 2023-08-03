63f3c42677f40a9931667d6f150927252e2b87ce
servanin <servanin88@gmail.com>
2023-08-03 15:49:57 +0300

    New

---
+++ tg-bot/src/test/java/baraholkateam/test/TestPhotoSend.java
@@ -0,0 +1,16 @@
+package baraholkateam.test;
+
+import org.telegram.telegrambots.meta.api.methods.ParseMode;
+import org.telegram.telegrambots.meta.api.methods.send.SendPhoto;
+import org.telegram.telegrambots.meta.api.objects.InputFile;
+
+import java.util.Objects;
+
+public class TestPhotoSend {
+    public static void main(String[] args) {
+
+
+
+
+    }
+}
