4f4842ee7f7e40ef754b3abbfacad5244cd25497
servanin <servanin88@gmail.com>
2023-08-03 14:08:47 +0300

    New

--- web/react-app/src/components/Profile.jsx
+++ web/react-app/src/components/Profile.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import '../style/style.css';
-import avatar from '../img/avatar.svg';
+//import avatar from '../img/avatar.svg';
 
 class Profile extends React.Component {
     constructor(props) {
@@ -13,7 +13,7 @@ class Profile extends React.Component {
             <div className="profile">
                 <div className="profile-photo-container">
                     <img className="profile-photo" alt="profile photo"
-                         src={userData.photo_url ? userData.photo_url : avatar}></img>
+                         src={userData.photo_url ? userData.photo_url : "/img/avatar"}></img>
                 </div>
                 <div className="profile-info">
                     <div className="user-name">{userData.first_name} {userData.last_name}</div>
